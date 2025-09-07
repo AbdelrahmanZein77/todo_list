dc6bcda89453cbd4547ba4401c7ecc03de2fd23d
abdelrahman zein <abdelrahmanzain29@gmail.com>
2025-09-07 22:24:40 +0300

    create token when create user

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
@@ -10,3 +15,8 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def CreateToken(sender, instance, created, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
