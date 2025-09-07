14aebb705244ce9e2259201bfab2991a44e7e08a
abdelrahman zein <abdelrahmanzain29@gmail.com>
2025-09-07 21:02:16 +0300

    add edit to project

--- tasks/models.py
+++ tasks/models.py
@@ -3,8 +3,10 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    complated = models.BooleanField(default=False)
     created_in = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField(auto_now_add=True, null=True, blank=True)
+    property = models.CharField(max_length=10, choices=[('low', 'Low'), ('medium', 'Medium'), ('high', 'High')], default='medium')
+    complated = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Task
 from .serializers import TaskSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filterset_fields = ['complated', 'due_date']
+
+    search_fields = ['title', 'description']
+
+    ordering_fields = ['due_date', 'created_in']
+    ordering = ['created_in']
